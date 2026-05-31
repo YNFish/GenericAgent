7fff53bf3d8816fb6b696fed365c10dc03c6902b
骄知乐 <1206739167@qq.com>
2026-05-31 20:43:51 +0800

    feat: initial commit - GenericAgent full codebase

---
+++ env.cmd
@@ -0,0 +1,10 @@
+
+@echo off
+set "PORTABLE_DEV_ROOT=C:\Users\xpc\GenericAgent\.portable"
+set "GENERICAGENT_HOME=C:\Users\xpc\GenericAgent"
+set "UV_PYTHON_INSTALL_DIR=C:\Users\xpc\GenericAgent\.portable\uv-python"
+set "UV_CACHE_DIR=C:\Users\xpc\GenericAgent\.portable\uv-cache"
+set "UV_DEFAULT_INDEX=https://pypi.tuna.tsinghua.edu.cn/simple"
+set "PIP_INDEX_URL=https://pypi.tuna.tsinghua.edu.cn/simple"
+set "PATH=C:\Users\xpc\GenericAgent\.portable\bin;C:\Users\xpc\GenericAgent\.portable\uv-python\cpython-3.12-windows-x86_64-none;C:\Users\xpc\GenericAgent\.portable\uv-python\cpython-3.12-windows-x86_64-none\Scripts;C:\Users\xpc\GenericAgent\.portable\tools\PortableGit\bin;C:\Users\xpc\GenericAgent\.portable\tools\PortableGit\usr\bin;%PATH%"
+echo Activated GenericAgent portable env: %GENERICAGENT_HOME%
---
+++ env.ps1
@@ -0,0 +1,9 @@
+﻿
+$env:PORTABLE_DEV_ROOT = "C:\Users\xpc\GenericAgent\.portable"
+$env:GENERICAGENT_HOME = "C:\Users\xpc\GenericAgent"
+$env:UV_PYTHON_INSTALL_DIR = "C:\Users\xpc\GenericAgent\.portable\uv-python"
+$env:UV_CACHE_DIR = "C:\Users\xpc\GenericAgent\.portable\uv-cache"
+$env:UV_DEFAULT_INDEX = "https://pypi.tuna.tsinghua.edu.cn/simple"
+$env:PIP_INDEX_URL = "https://pypi.tuna.tsinghua.edu.cn/simple"
+$env:PATH = "C:\Users\xpc\GenericAgent\.portable\bin;C:\Users\xpc\GenericAgent\.portable\uv-python\cpython-3.12-windows-x86_64-none;C:\Users\xpc\GenericAgent\.portable\uv-python\cpython-3.12-windows-x86_64-none\Scripts;C:\Users\xpc\GenericAgent\.portable\tools\PortableGit\bin;C:\Users\xpc\GenericAgent\.portable\tools\PortableGit\usr\bin;$env:PATH"
+Write-Host "Activated GenericAgent portable env: $env:GENERICAGENT_HOME" -ForegroundColor Green
